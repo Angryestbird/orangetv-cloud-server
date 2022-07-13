cde8cb3270b7b67b267082138343bc30759fc5fc
nightCat <angryestbird@sina.com>
2022-07-13 18:36:51 +0800

    initial commit

---
+++ orangetv-cloud-server-discovery/src/test/java/com/orangetv/cloud/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangetv.cloud.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
